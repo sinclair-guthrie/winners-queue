4876d326b2c59e8c73e0040a1558ae1d5c8f82b3
Sinclair Guthrie <sinclair.guthrie@gmail.com>
2020-09-24 16:55:51 -0400

    Updates

---
+++ precache-manifest.f3fa11f9f87c9696da90a01bef2f7f1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548a9f42814ac1a7707053d222824bac",
+    "url": "/winners-queue/index.html"
+  },
+  {
+    "revision": "a685ec9619983a2aa5d5",
+    "url": "/winners-queue/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6ba491a3d854fd6f248",
+    "url": "/winners-queue/static/js/2.2d3d8c77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/winners-queue/static/js/2.2d3d8c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a685ec9619983a2aa5d5",
+    "url": "/winners-queue/static/js/main.2578061d.chunk.js"
+  },
+  {
+    "revision": "6084916f6591a1af2724",
+    "url": "/winners-queue/static/js/runtime-main.c8c1f7db.js"
+  }
+]);
